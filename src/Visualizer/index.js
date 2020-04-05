f5e69b84e3e99b93c6decea1f818a6822f1b3ba5
aaigarayev <alimzhan.aigarayev@gmail.com>
2020-04-05 18:48:38 +0200

    layout created

--- src/Visualizer/index.js
+++ src/Visualizer/index.js
@@ -18,9 +18,11 @@ export default class Visualizer extends Component {
 
     resetArray() {
         const array = [];
+        // empty an array in case there is something left
+        this.setState({array});
         
         const barCounter = (counter) => {
-            if (counter < 200) {
+            if (counter < 300) {
                 setTimeout(() => {
                     // adding random number using the external function
                     array.push(randomIntFromInterval(5, 600));
@@ -29,7 +31,7 @@ export default class Visualizer extends Component {
                     counter++;
                     // recursively calling the function
                     barCounter(counter);
-                }, 50);
+                }, 25);
             } 
         }
         barCounter(0)
@@ -39,18 +41,59 @@ export default class Visualizer extends Component {
         const {array} = this.state;
 
         return (
-            <div className="main-container">
-                <div className="array-container">
-                    {array.map((value, idx) => (
-                        <div 
-                        className="array-bar" 
-                        key={idx}
-                        style={{height: `${value}px`,
-                                marginTop: `${600 - value}px`}}>
-                        </div>
-                    ))}
+            <React.Fragment>
+                <div className='btn-container'>
+                    <button 
+                    className='sort-btn'
+                    onClick={() => this.resetArray()} 
+                    disabled={array.length < 300}>
+                        Generate a new array
+                    </button>
+                    <button 
+                    className='sort-btn'
+                    onClick={() => console.log('Merge sort')} 
+                    disabled={array.length < 300}>
+                        Merge sort
+                    </button>
+                    <button 
+                    className='sort-btn'
+                    onClick={() => console.log('Bubble sort')} 
+                    disabled={array.length < 300}>
+                        Bubble sort
+                    </button>
+                    <button 
+                    className='sort-btn'
+                    onClick={() => console.log('Insertion sort')} 
+                    disabled={array.length < 300}>
+                        Insertion sort
+                    </button>
+                    <button 
+                    className='sort-btn'
+                    onClick={() => console.log('Selection sort')} 
+                    disabled={array.length < 300}>
+                        Selection sort
+                    </button>
+                    <button 
+                    className='sort-btn'
+                    onClick={() => console.log('Quicksort')} 
+                    disabled={array.length < 300}>
+                        Quicksort
+                    </button>
                 </div>
-            </div>
+                <div className="main-container">
+                    <div className="array-container">
+                        {array.map((value, idx) => (
+                            <div 
+                            className="array-bar" 
+                            key={idx}
+                            // giving the height using their value which is selected randomly
+                            style={{height: `${value}px`,
+                            marginTop: `${620 - value}px`}}>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
